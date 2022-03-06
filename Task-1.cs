7277217217ae9a8e8e3454cf95b427d018993291
TasksAmiraslanov <tu7mxp4w4@code.edu.az>
2022-03-06 19:33:15 +0400

    Task-1 added

---
+++ Task-1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Cumleni daxil edin :");
+           string Sentence=Console.ReadLine();
+           Console.WriteLine("Axtardiginiz herfi  daxil edin :");
+           char letter=Convert.ToChar(Console.ReadLine());
+           Word(Sentence,letter); 
+        }
+        static void Word (string Sentence,char letter,int count=0){
+        
+        for (int j = 0; j<Sentence.Length ; j ++)
+        {
+           if(Sentence[j] == letter) 
+           {
+              count++;
+           }
+  
+        }
+       Console.WriteLine($"Herflerin sayi : {count}");
+       return;
+       
+
+      
+}
+}
+}
+
+
